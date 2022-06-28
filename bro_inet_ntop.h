a60ffe6a41c0f335f1dcf22ec2e6bf246b9ab0ac
Tim Wojtulewicz <tim@corelight.com>
2022-06-28 16:02:00 -0700

    Deprecate bro_inet_ntop.{c,h}

--- bro_inet_ntop.h
+++ bro_inet_ntop.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#warning "This file is deprecated and will be removed in v6.1. Use zeek_inet_ntop.h instead."
+#include "zeek_inet_ntop.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -7,7 +10,8 @@ extern "C" {
 #include <sys/types.h>
 #include <sys/socket.h>
 
-const char * 
+[[deprecated("Remove in v6.1. Use zeek_inet_ntop.")]]
+const char *
 bro_inet_ntop(int af, const void * __restrict src, char * __restrict dst,
     socklen_t size);
 
